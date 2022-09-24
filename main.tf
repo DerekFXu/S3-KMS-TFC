3368280da697a8a4ad2765c4d3d5666a81cdcfb8
Derek Xu <derekfxu@gmail.com>
2022-09-24 17:27:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket" "dfxbucket22" {
   bucket = "dfxbucket22"
 }
 
+resource "aws_s3_bucket_acl" "acl_private" {
+  bucket = aws_s3_bucket.dfxbucket22
+  acl = "private"   
+}
 resource "aws_s3_bucket_ownership_controls" "bucket_controls" {
   bucket = aws_s3_bucket.dfxbucket22.id
 
