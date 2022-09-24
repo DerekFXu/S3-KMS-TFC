5f53acd7777492d3a064a9f69cbccec607da194b
Derek Xu <derekfxu@gmail.com>
2022-09-24 17:10:13 -0400

    bucket test 2

--- main.tf
+++ main.tf
@@ -2,27 +2,14 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+resource "aws_s3_bucket" "dfxbucket22" {
+  bucket = "dfxbucket22"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.dfxbucket22.id
 
-  tags = {
-    Name = var.instance_name
+  rule {
+    object_ownership = "BucketOwnerEnforced"
   }
 }
