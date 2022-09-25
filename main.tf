f3f2030ec8ad728ca7dab7cc4f8c88ae563b983b
Derek Xu <derekfxu@gmail.com>
2022-09-25 09:23:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "dfxbucket22" {
   bucket = "dfxbucket22"
-}
-
-resource "aws_s3_bucket_ownership_controls" "disable_s3_acl" {
-  bucket = "dfxbucket22"
-
+    
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
