fcd147a33f524b92b3a754f8619a84dc4fb2edab
Derek Xu <derekfxu@gmail.com>
2022-09-25 09:55:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,15 @@ resource "aws_s3_bucket" "dfxbucket22" {
   bucket = "dfxbucket22"
 }
 
+resource "aws_s3_bucket_public_access_block" "public_block" {
+  bucket = aws_s3_bucket.dfxbucket22.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_acl" "dfxbucket22-acl" {
   bucket = aws_s3_bucket.dfxbucket22.id
   acl = "private"
@@ -13,10 +22,8 @@ resource "aws_s3_bucket_acl" "dfxbucket22-acl" {
 
 resource "aws_s3_bucket_ownership_controls" "dfxbucket22-ownership" {
   bucket = aws_s3_bucket.dfxbucket22.id
+  
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
-  depends_on = [
-    aws_s3_bucket_acl.dfxbucket22-acl
-]
 }
