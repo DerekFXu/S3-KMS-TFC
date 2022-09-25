305c07b61c1860e9bde79ca696b28920680432d8
Derek Xu <derekfxu@gmail.com>
2022-09-25 10:07:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_kms_key" "mykey" {
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "dfxbucket22" {
   bucket = "dfxbucket22"
 }
@@ -22,8 +26,19 @@ resource "aws_s3_bucket_acl" "dfxbucket22-acl" {
 
 resource "aws_s3_bucket_ownership_controls" "dfxbucket22-ownership" {
   bucket = aws_s3_bucket.dfxbucket22.id
-  
+
   rule {
     object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "kms-s3-encryption" {
+  bucket = aws_s3_bucket.dfxbucket22.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
   }
 }
